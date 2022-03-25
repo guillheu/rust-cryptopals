0a69350bec461b48b2fb40262058dc380aeacc61
guillheu <guillheu@gmail.com>
2022-03-25 07:40:48 +0100

    started refactoring Vec<u8> into a newtype bytes

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,10 @@ extern crate thiserror;
 
 use thiserror::Error;
 
-/// This alias will make it easier to handle bytes for cryptographic operations.
-type Bytes = Vec<u8>;
+/// This newtype will make it easier to handle bytes for cryptographic operations.
+/// A newtype is preferable to an alias, to avoid having bytes-specific methods apply too freely with other Vec<u8>-based data types, and vice-versa.
+/// e.g. a method for Bytes will not work for instance on a `type Ascii = Vec<u8>`, which is a good thing : )
+struct Bytes(Vec<u8>);
 
 #[derive(Error, Debug)]
 #[error("{encoding} : {message}")]
